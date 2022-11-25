52edfc9e6b18fcb4974a96e1c43e541972bce7fe
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-11-25 09:41:40 +0200

    for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
